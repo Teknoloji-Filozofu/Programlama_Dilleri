9d5cad63f89f6848e2fe3429811fc610e7bb778f
Sadık Oktay Biçici <s.oktay.bicici@gmail.com>
2023-10-11 16:39:30 +0300

    delege işlemi eklendi
    Co-Authored-By: Sadık Oktay Biçici <s.oktay.bicici@gmail.com>
    Co-Authored-By: Hamdi Anıl Hacıhaliloğlu <96963553+hamdihacihaliloglu@users.noreply.github.com>

---
+++ C_Sharp_Programlama/13-Delege/Faiz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegele
+{
+    class Faiz
+    {
+        public delegate void FaizHesapla(decimal Tutar, int Vade);
+
+        public void GunlukFaiz(decimal Anapara, int Vade)
+        {
+            Console.WriteLine($"{Vade} Günlük Kazancınız: {Anapara * 7 * Vade / 36500}");
+        }
+
+        public void AylikFaiz(decimal Anapara, int Vade)
+        {
+            Console.WriteLine($"{Vade} Aylık Kazancınız: {Anapara * 10 * Vade / 1200}");
+        }
+
+        public void YillikFaiz(decimal Anapara, int Vade)
+        {
+            Console.WriteLine($"{Vade} Yıllık Kazancınız : {Anapara * 15 * Vade / 100}");
+        }
+    }
+}
---
+++ C_Sharp_Programlama/13-Delege/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegele
+{
+    class Program
+    {
+
+        //delegeler kullanılan metodların dönüş tipinde olmak zorundadır.
+        delegate void PizzaYap();
+        static void Main(string[] args)
+        {
+            PizzaYap pizzaYap = new PizzaYap(StandartMalzeme);
+            pizzaYap += SucukluPizza;
+            pizzaYap += VejeteryanPizza;
+            pizzaYap -= SucukluPizza;
+
+            pizzaYap();
+
+
+            //delegeler parametre olarak da verilebilir
+            PizzaYapMetodu(SucukluPizza);
+
+
+
+            Faiz faizSinif = new Faiz();
+            Faiz.FaizHesapla faizHesapla;
+            faizHesapla = faizSinif.GunlukFaiz;
+            faizHesapla += faizSinif.AylikFaiz;
+            faizHesapla += faizSinif.YillikFaiz;
+
+            faizHesapla(10000,12);
+
+            Console.ReadLine();
+
+        }
+
+        static void PizzaYapMetodu(PizzaYap pizzaMetodu)
+        {
+            pizzaMetodu += StandartMalzeme;
+            pizzaMetodu();
+        }
+
+        static void StandartMalzeme()
+        {
+            Console.WriteLine("Hamur eklendi.");
+            Console.WriteLine("Domates sosu eklendi.");
+            Console.WriteLine("Kaşar Peyniri eklendi.");
+        }
+
+        static void SucukluPizza()
+        {
+            Console.WriteLine("Sucuk eklendi.");
+            Console.WriteLine("Baharat eklendi.");
+        }
+
+        static void VejeteryanPizza()
+        {
+            Console.WriteLine("Dere Otu eklnedi.");
+            Console.WriteLine("Baharat eklendi.");
+        }
+
+        static void TonBalikliPizza()
+        {
+            Console.WriteLine("Ton balığı eklendi.");
+            Console.WriteLine("Dere otu eklendi.");
+        }
+    }
+}
