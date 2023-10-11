a6328cc707aba9d0009a6188145e11dc0e1a409d
Sadık Oktay Biçici <s.oktay.bicici@gmail.com>
2023-10-11 16:31:22 +0300

    fonksiyonlar eklendi
    Co-Authored-By: Sadık Oktay Biçici <s.oktay.bicici@gmail.com>
    Co-Authored-By: Hamdi Anıl Hacıhaliloğlu <96963553+hamdihacihaliloglu@users.noreply.github.com>

---
+++ C_Sharp_Programlama/7-Metodlar-(Fonksiyonlar)/metodlarda-overload.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodlarda_overloaf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Topla(3, 5);
+            Topla(3, 5, 6);
+        }
+
+        static void Topla(int say1, int say2)
+        {
+            Console.WriteLine(say1+say2);
+        }
+
+        static void Topla(int say1, int say2,int say3)
+        {
+            Console.WriteLine(say1 + say2 + say3);
+        }
+
+    }
+}
---
+++ C_Sharp_Programlama/7-Metodlar-(Fonksiyonlar)/ref-out-parametreleri.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ref_out_parametreleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aa = 10;
+
+            Console.WriteLine("Par' a girmeden önce: " + aa);
+            Par(aa);
+            Console.WriteLine("Par' a girdikten sonra: " + aa);
+
+            Console.WriteLine("----------------------------");
+
+            Console.WriteLine("ParRef' a girmeden önce: " + aa);
+            ParRef(ref aa);
+            Console.WriteLine("ParRef' a girdikten sonra: " + aa);
+
+            Console.WriteLine("----------------------------");
+
+            Console.WriteLine("ParOut' a girmeden önce: " + aa);
+            ParOut(out aa);
+            Console.WriteLine("ParOut' a girdikten sonra: " + aa);
+
+            Console.ReadLine();
+        }
+
+        static void Par(int x)
+        {
+            x = x * 2;
+        }
+
+        //referansını alır ve değişikliği gerçekleştirir
+        static void ParRef(ref int x)
+        {
+            x = x * 2;
+        }
+
+        static void ParOut(out int y)
+        {
+            y = 2;
+        }
+    }
+}
