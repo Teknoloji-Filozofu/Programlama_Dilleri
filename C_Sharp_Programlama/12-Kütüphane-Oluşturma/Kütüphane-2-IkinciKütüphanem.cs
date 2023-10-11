0af5ee365541387bbba16282f674ee7ae6431e25
Sadık Oktay Biçici <s.oktay.bicici@gmail.com>
2023-10-11 16:36:49 +0300

    kütüphane oluşturma eklendi
    Co-Authored-By: Sadık Oktay Biçici <s.oktay.bicici@gmail.com>
    Co-Authored-By: Hamdi Anıl Hacıhaliloğlu <96963553+hamdihacihaliloglu@users.noreply.github.com>

---
+++ C_Sharp_Programlama/12-Kütüphane-Oluşturma/Kütüphane-2-IkinciKütüphanem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sınıf_Kutuphanesi_2
+{
+    public class IkinciKutuphanem
+    {
+        public void HosGeldin()
+        {
+            MessageBox.Show("Sınıf Kütüphanesi - 2 Projesine Hoşgeldiniz.");
+        }
+
+    }
+}
---
+++ C_Sharp_Programlama/12-Kütüphane-Oluşturma/main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kutuphane1 = Sınıf_Kutuphanesi;
+using Kutuphane2 = Sınıf_Kutuphanesi_2;
+using ErişimBelirleyicileri;
+
+namespace SınıflarıCagirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kutuphane2.IlkKutuphanem kutuphane = new Kutuphane2.IlkKutuphanem();
+            Kutuphane2.IkinciKutuphanem kutuphane2 = new Kutuphane2.IkinciKutuphanem();
+         
+            kutuphane.HosGeldin();
+            kutuphane2.HosGeldin();
+            PublicSinif Sinif=new PublicSinif();
+            MetotErisimBelirleyicileri erisim = new MetotErisimBelirleyicileri();
+   
+        }
+    }
+}
