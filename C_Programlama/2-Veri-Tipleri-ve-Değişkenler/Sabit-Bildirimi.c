9dc88d1e7ab5648e7bb386ada727223639f9a0ca
Sadık Oktay Biçici <s.oktay.bicici@gmail.com>
2023-10-15 13:57:47 +0300

    veri-tipleri-ve-değişkenler

---
+++ C_Programlama/2-Veri-Tipleri-ve-Değişkenler/Sabit-Bildirimi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define UZUN 10   
+#define KISA  5
+#define NEWLINE '\n'
+
+int main() {
+   int alan;  
+  
+   alan = UZUN * KISA;
+   printf(" Alan : %d ",alan);
+   printf(" %c ", NEWLINE);
+
+   return 0;
+}
+
