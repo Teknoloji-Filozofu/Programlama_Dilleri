6c2f9fc56747482d404d78bf4fecaf5d9156dd72
Sadık Oktay Biçici <s.oktay.bicici@gmail.com>
2024-02-09 13:53:21 +0300

    Update class.py

--- Python_Programlama/11-Nesne-Yönelimli-Programlama/class.py
+++ Python_Programlama/11-Nesne-Yönelimli-Programlama/class.py
@@ -29,3 +29,22 @@ print(personel1)
 perint(personel1.ad,personel1,soyad)
 
 """
+
+
+"""
+
+class calisan:
+    def __init__(self,ad,soyad,maas)
+        self.ad = ad
+        self.soyad = soyad
+        self.maas = maas
+    def tamad(self):
+        return "adı : {} soyadı {} :".format(self.ad,self.soyad)
+personel1 = calisan("ali","demir",2500)
+personel2 = calisan("kerim","diyar"2350)
+
+print(personel1)
+print(personel.ad,personel.soyad)
+print(personel.tamad())
+
+"""
