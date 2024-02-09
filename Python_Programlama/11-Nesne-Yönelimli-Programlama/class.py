dd314e43917547ce4c619ece28ddb4cfcc851e23
Sadık Oktay Biçici <s.oktay.bicici@gmail.com>
2024-02-09 10:38:47 +0300

    Update class.py

--- Python_Programlama/11-Nesne-Yönelimli-Programlama/class.py
+++ Python_Programlama/11-Nesne-Yönelimli-Programlama/class.py
@@ -9,3 +9,23 @@ print(ogrenci.isim)
 print(ogrenci.soyisim)
 print(ogrenci.yas)
 print(ogrenci.not_ort)
+
+
+"""
+class calisan:
+    pass
+
+personel1 = calisan()
+personel1.ad = "ali"
+personel1.soyad = "demir"
+personel1.maas = 2500
+
+personel2 = calisan()
+personel2.ad = "kerim"
+personel2.soyad = "diyar"
+personel2.maas = 2350
+
+print(personel1)
+perint(personel1.ad,personel1,soyad)
+
+"""
